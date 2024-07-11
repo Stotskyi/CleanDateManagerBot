a99b79f9d022acecf82ff098343b6351a2b2cf40
Stotskyi <andrii.stotskyi.u@gmail.com>
2024-07-11 14:23:17 +0300

    solve problem with commands

--- Picker.Application/Abstractions/CommandFactory.cs
+++ Picker.Application/Abstractions/CommandFactory.cs
@@ -11,11 +11,11 @@ public class CommandFactory(IColiverRepository coliverRepository, ITelegramBotCl
     public ICommand GetCommand(string messageText) =>
         messageText switch
         {
-            var text when text == "/enroll" => new EnrollCommand(),
-            var text when text == "/remove" => new RemoveCommand(),
-            var text when text == "/table" => new TableCommand(coliverRepository, botClient),
-            var text when text == "/cleaner" => new CleanerCommand(coliverRepository),
-            var text when text == "/time" => new TimeCommand(coliverRepository),
-            var text when text == "/generateCycle" => new GenerateCycleCommand(coliverRepository)
+            var text when text.StartsWith("/enroll") => new EnrollCommand(),
+            var text when text.StartsWith("/remove") => new RemoveCommand(),
+            var text when text.StartsWith("/table") => new TableCommand(coliverRepository, botClient),
+            var text when text.StartsWith("/cleaner") => new CleanerCommand(coliverRepository),
+            var text when text.StartsWith("/time") => new TimeCommand(coliverRepository),
+            var text when text.StartsWith("/generateCycle") => new GenerateCycleCommand(coliverRepository)
         };
 }
--- Picker/obj/Debug/net8.0/Picker.AssemblyInfo.cs
+++ Picker/obj/Debug/net8.0/Picker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Picker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95a7f592b480ca58b815cffb6a224a580a3634e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b4c828d3fe2df5ae452d6a0dae3d4f5505518b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Picker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Picker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
